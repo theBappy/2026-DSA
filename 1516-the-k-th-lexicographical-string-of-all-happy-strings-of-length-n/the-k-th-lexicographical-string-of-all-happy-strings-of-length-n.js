16d56483f12bac8b3b2a7cbca42fbb5f25f9c754
the_Bappy <abappy575@gmail.com>
2026-03-14 08:21:11 +0600

    Time: 31 ms (21.05%) | Memory: 61.4 MB (14.03%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
@@ -1,25 +1,2 @@
-var getHappyString = function(n, k) {
-    const sz = 3 * (1 << (n - 1));
-    if (k > sz) return "";
-
-    const base = 1 << (n - 1);
-    const q = Math.floor((k - 1) / base);
-    const r = (k - 1) % base;
-
-    const s = new Array(n);
-    s[0] = String.fromCharCode('a'.charCodeAt(0) + q);
-
-    const xx = [
-        ['b', 'c'],
-        ['a', 'c'],
-        ['a', 'b']
-    ];
-
-    for (let i = n - 2; i >= 0; i--) {
-        const idx = s[n - 2 - i].charCodeAt(0) - 'a'.charCodeAt(0);
-        const bit = (r >> i) & 1;
-        s[n - 1 - i] = xx[idx][bit];
-    }
-
-    return s.join("");
-};
+const getHappyString = f = (n, k, list = ['']) =>
+    n ? f(n - 1, k, list.flatMap(v => [...'abc'].map(c => !v.endsWith(c) ? v + c : '').filter($ => $))) : list[k - 1] || '';
