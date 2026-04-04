f814c720a42dae6b15fab0dd3d444d2c2fdfb5b8
the_Bappy <abappy575@gmail.com>
2026-04-04 11:37:04 +0600

    Time: 47 ms (20.34%) | Memory: 46.1 MB (37.29%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -4,7 +4,9 @@ public:
         int l = encodedText.length();
         int columns = l / rows;
         string originalText;
-
+        
+        // T.C : O(l), l = encodedText.length()
+        // S.C : O(1)
         for (int col = 0; col < columns; col++) {
             for (int j = col; j < l; j += (columns + 1)) {
                 originalText += encodedText[j];
