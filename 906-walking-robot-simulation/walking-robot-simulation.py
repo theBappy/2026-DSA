7c7dd3c8147e602e66b71a6eecd97d74926e2490
the_Bappy <abappy575@gmail.com>
2026-04-06 22:14:31 +0600

    Time: 23 ms (96.52%) | Memory: 23.7 MB (69.44%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -1,24 +1,27 @@
 class Solution:
-    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
-        st = set()
-        for obs in obstacles:
-            key = f"{obs[0]}_{obs[1]}"
-            st.add(key)
+    def robotSim(self, commands: list[int], obstacles: list[list[int]]) -> int:
+        # Using a set of tuples is much faster than strings
+        st = {tuple(obs) for obs in obstacles}
+        
         x, y = 0, 0
         maxD = 0
-        dir_x, dir_y = 0, 1
-        for command in commands:
-            if command == -2:
-                dir_x, dir_y = -dir_y, dir_x
-            elif command == -1:
-                dir_x, dir_y = dir_y, -dir_x
+        dx, dy = 0, 1 # Starting North
+        
+        for cmd in commands:
+            if cmd == -2: # Left 90 degrees
+                dx, dy = -dy, dx
+            elif cmd == -1: # Right 90 degrees
+                dx, dy = dy, -dx
             else:
-                for _ in range(command):
-                    newX = x + dir_x
-                    newY = y + dir_y
-                    newKey = f"{newX}_{newY}"
-                    if newKey in st:
+                for _ in range(cmd):
+                    # Check if next step is an obstacle
+                    if (x + dx, y + dy) in st:
                         break
-                    x, y = newX, newY
-            maxD = max(maxD, x * x + y * y)
+                    x += dx
+                    y += dy
+                
+                # Update max distance after the move command is finished
+                # (x*x + y*y) is the squared distance
+                maxD = max(maxD, x*x + y*y)
+                
         return maxD
