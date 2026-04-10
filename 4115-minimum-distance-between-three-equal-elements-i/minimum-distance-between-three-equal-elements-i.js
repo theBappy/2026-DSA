f5683b31b12c1e92f8a97d347b388dbc0a45d9d0
the_Bappy <abappy575@gmail.com>
2026-04-10 10:40:16 +0600

    Time: 3 ms (57.14%) | Memory: 58.5 MB (9.52%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.js
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.js
@@ -1,16 +1,18 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var minimumDistance = function (nums) {
-    const n = nums.length;
-    if (n <= 2) return -1;
-    let ans = Infinity;
-    for (let i = 0; i < n; i++)
-        for (let j = i + 1; j < n; j++)
-            if (nums[i] === nums[j])
-                for (let k = j + 1; k < n; k++)
-                    if (nums[j] === nums[k])
-                        ans = Math.min(ans, 2 * (k - i));
-    return ans === Infinity ? -1 : ans;
+    const pos = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        if (!pos.has(nums[i])) {
+            pos.set(nums[i], [])
+        }
+        pos.get(nums[i]).push(i)
+    }
+    let ans = Infinity
+    for (const [val, indices] of pos.entries()) {
+        if (indices.length >= 3) {
+            for (let i = 0; i <= indices.length - 3; i++) {
+                ans = Math.min(ans, 2 * (indices[i + 2] - indices[i]))
+            }
+        }
+    }
+    return ans === Infinity ? -1 : ans
 };
