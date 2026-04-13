c413779da9fcab56d6e88a7500e2d8dccbd89511
the_Bappy <abappy575@gmail.com>
2026-04-13 10:33:53 +0600

    Time: 0 ms (100.00%) | Memory: 13.4 MB (16.70%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int result = INT_MAX;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n && result > abs(i - start); i++) {
             if (nums[i] == target) {
                 result = min(result, abs(i - start));
             }
