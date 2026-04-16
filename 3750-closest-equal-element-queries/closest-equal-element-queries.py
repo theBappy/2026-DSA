9242d4a3d8e45846defe681306de29234e2203e2
the_Bappy <abappy575@gmail.com>
2026-04-16 13:08:15 +0600

    Time: 319 ms (67.19%) | Memory: 54.7 MB (78.91%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.py
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.py
@@ -16,11 +16,6 @@ class Solution:
             if m == 1:
                 continue
             for i in range(m):
-
-                prev_idx_distance = min(abs(idx[i] - idx[(i - 1 + m) % m]), n - abs(idx[i] - idx[(i - 1 + m) % m]))
-
-                next_idx_distance = min(abs(idx[i] - idx[(i + 1) % m]), n - abs(idx[i] - idx[(i + 1) % m]))
-
-                ans[idx[i]] = min(prev_idx_distance, next_idx_distance)
+                ans[idx[i]] = min(min(abs(idx[i] - idx[(i - 1 + m) % m]), n - abs(idx[i] - idx[(i - 1 + m) % m])), min(abs(idx[i] - idx[(i + 1) % m]), n - abs(idx[i] - idx[(i + 1) % m])))
 
         return [ans[idx] for idx in queries]
