eec346f6c95bfa807072c725d9a902cb929c5eb6
theBappy <abappy575@gmail.com>
2026-04-17 18:03:03 +0600

    Time: 99 ms (87.75%) | Memory: 131.9 MB (75.49%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int minMirrorPairDistance(vector<int>& nums) {
-        auto reverseNum = [](int x) {
-            int rev = 0;
-            while (x > 0) {
-                rev = rev * 10 + (x % 10);
-                x /= 10;
-            }
-            return rev;
-        };
-        unordered_map<int, int> last_seen;
-        int ans = INT_MAX;
-        for (int i = 0; i < nums.size(); i++) {
-            int current = nums[i];
-            if (last_seen.count(current)) {
-                ans = min(ans, i - last_seen[current]);
-            }
-            last_seen[reverseNum(current)] = i;
+    int getReverse(int n) {
+        int reversed = 0;
+        while (n > 0) {
+            int remaining = n % 10;
+            reversed = reversed * 10 + remaining;
+            n /= 10;
         }
-        return ans == INT_MAX ? -1 : ans;
+        return reversed;
+    }
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int, int> mp; // reversed -> idx
+        int result = INT_MAX;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(nums[i])) {
+                result = min(result, i - mp[nums[i]]);
+            }
+            mp[getReverse(nums[i])] = i;
+        }
+        return result == INT_MAX ? -1 : result;
     }
 };
