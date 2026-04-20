a4fc55ff55c81a31bdd63bd7b5cb4472a65006c9
theBappy <abappy575@gmail.com>
2026-04-20 09:20:00 +0600

    Time: 0 ms (100.00%) | Memory: 53.3 MB (89.47%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.js
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.js
@@ -1,20 +1,21 @@
+/**
+ * @param {number[]} colors
+ * @return {number}
+ */
 var maxDistance = function (colors) {
     const n = colors.length
     let res = 0
-
-    for (let j = n - 1; j > 0; j--) {
+    for (let j = n - 1; j >= 0; j--) {
         if (colors[0] !== colors[j]) {
             res = Math.max(res, j)
             break
         }
     }
-
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < n; i++) {
         if (colors[i] !== colors[n - 1]) {
             res = Math.max(res, n - 1 - i)
             break
         }
     }
-
     return res
 };
