ea5dc343eb6aebdade11029e094c8eab632beb54
theBappy <abappy575@gmail.com>
2026-04-20 09:39:30 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.41%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
@@ -3,7 +3,10 @@ class Solution:
         n = len(colors)
         res = 0
         for i in range(n):
-            for j in range(i + 1, n):
-                if colors[i] != colors[j]:
-                    res = max(res, j - i)
+            # i in any index compare with left most(0'th house)
+            if colors[i] != colors[0]:
+                res = max(res, i)
+            # i in any index compare with right most(n-1'th house)
+            if colors[i] != colors[n - 1]:
+                res = max(res, abs(i - (n - 1)))
         return res
