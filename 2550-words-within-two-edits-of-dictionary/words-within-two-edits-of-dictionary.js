339d53b21bec4da8ecb5d90d3c0d09446877b2d7
theBappy <abappy575@gmail.com>
2026-04-22 09:25:54 +0600

    Time: 24 ms (15.00%) | Memory: 57.5 MB (45.00%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.js
@@ -0,0 +1,18 @@
+var twoEditWords = function (queries, dictionary) {
+    const res = [];
+    for (const query of queries) {
+        for (const word of dictionary) {
+            let hamming_dist = 0;
+            for (let i = 0; i < query.length; i++) {
+                if (query[i] !== word[i]) {
+                    hamming_dist++;
+                }
+            }
+            if (hamming_dist <= 2) {
+                res.push(query);
+                break;
+            }
+        }
+    }
+    return res;
+};
