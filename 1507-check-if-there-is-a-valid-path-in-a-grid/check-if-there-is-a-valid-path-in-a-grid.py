0c552e976d615feafece1c7c9dc0bea11e2fffe8
theBappy <abappy575@gmail.com>
2026-04-27 09:06:38 +0600

    Time: 195 ms (61.57%) | Memory: 34.3 MB (77.24%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,41 +1,36 @@
+from collections import deque
+
+
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         m, n = len(grid), len(grid[0])
 
-        parent = list(range(m * n))
-        rank = [0] * (m * n)  # Initialize ranks to zero
+        dirs = {
+            1: [(0, -1), (0, 1)],
+            2: [(-1, 0), (1, 0)],
+            3: [(0, -1), (1, 0)],
+            4: [(0, 1), (1, 0)],
+            5: [(0, -1), (-1, 0)],
+            6: [(0, 1), (-1, 0)],
+        }
 
-        def find(i):
-            # Path compression: makes every node point to the root
-            if parent[i] != i:
-                parent[i] = find(parent[i])
-            return parent[i]
+        que = deque([(0, 0)])
+        visited = set([(0, 0)])
 
-        def union(i, j):
-            root_i, root_j = find(i), find(j)
-            if root_i != root_j:
-                # Union by Rank: attach the smaller tree under the larger tree
-                if rank[root_i] > rank[root_j]:
-                    parent[root_j] = root_i
-                elif rank[root_i] < rank[root_j]:
-                    parent[root_i] = root_j
-                else:
-                    parent[root_j] = root_i
-                    rank[root_i] += 1
+        while que:
+            r, c = que.popleft()
 
-        for r in range(m):
-            for c in range(n):
-                curr_idx = r * n + c
-                curr_type = grid[r][c]
+            if (r, c) == (m - 1, n - 1):
+                return True
 
-                # Check Right Connection
-                if c + 1 < n:
-                    if curr_type in {1, 4, 6} and grid[r][c + 1] in {1, 3, 5}:
-                        union(curr_idx, curr_idx + 1)
+            for dx, dy in dirs[grid[r][c]]:
+                nr, nc = r + dx, c + dy
 
-                # Check Down Connection
-                if r + 1 < m:
-                    if curr_type in {2, 3, 4} and grid[r + 1][c] in {2, 5, 6}:
-                        union(curr_idx, curr_idx + n)
-
-        return find(0) == find(m * n - 1)
+                if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in visited:
+                    # check reverse connection
+                    for x, y in dirs[grid[nr][nc]]:
+                        if nr + x == r and nc + y == c:
+                            visited.add((nr, nc))
+                            que.append((nr, nc))
+                            break
+        return False
