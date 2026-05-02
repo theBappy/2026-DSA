85f539a38000ce95a4e941db08a0ebe6a36d756e
theBappy <abappy575@gmail.com>
2026-05-02 15:06:41 +0600

    Time: 1 ms (95.24%) | Memory: 56 MB (71.43%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.ts
@@ -0,0 +1,18 @@
+function rotatedDigits(n: number): number {
+    const memo = new Uint8Array(n + 1)
+    let count = 0
+
+    for (let i = 1; i <= n; i++) {
+        const remainingPart = memo[Math.floor(i / 10)]
+        const digit = i % 10
+        if (remainingPart === 2 || digit === 3 || digit === 4 || digit === 7) {
+            memo[i] = 2
+        } else if (remainingPart === 1 || digit === 2 || digit === 5 || digit === 6 || digit === 9) {
+            memo[i] = 1
+            count++
+        } else {
+            memo[i] = 0
+        }
+    }
+    return count
+};
