868894a56790acaf0af1635e42b6035b27475386
theBappy <abappy575@gmail.com>
2026-05-02 14:18:02 +0600

    Time: 0 ms (100.00%) | Memory: 9.6 MB (18.77%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int rotatedDigits(int n) {
-        int cnt = 0;
+        vector<int> memo_arr(n + 1, -1);
+        memo_arr[0] = 0;
+        int count = 0;
+
         for (int i = 1; i <= n; i++) {
-            int check = i;
-            bool valid = true;
-            bool changed = false;
-            while (check > 0 && valid) {
-                int digit = check % 10;
-                if (digit == 3 || digit == 4 || digit == 7)
-                    valid = false;
-                else if (digit == 2 || digit == 5 || digit == 6 || digit == 9)
-                    changed = true;
-                check /= 10;
+            int remaining_part = memo_arr[i / 10];
+            if (remaining_part == 2) {
+                memo_arr[i] = 2;
+                continue;
             }
-            if (valid && changed)
-                cnt++;
+
+            int digit = i % 10;
+            int digit_check;
+            if (digit == 0 || digit == 1 || digit == 8)
+                digit_check = 0; // same number
+            else if (digit == 2 || digit == 5 || digit == 6 || digit == 9)
+                digit_check = 1; // good number
+            else {
+                memo_arr[i] = 2; // invalid case
+                continue;
+            }
+            if (remaining_part == 0 && digit_check == 0)
+                memo_arr[i] = 0;
+            else
+                memo_arr[i] = 1;
+
+            if (memo_arr[i] == 1)
+                count++;
         }
-        return cnt;
+        return count;
     }
 };
