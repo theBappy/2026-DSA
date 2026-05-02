f28f7b79b7fbb782ba9273b0786faa6ae23b7ad2
theBappy <abappy575@gmail.com>
2026-05-02 11:16:31 +0600

    Time: 39 ms (35.06%) | Memory: 19.5 MB (14.49%) - LeetSync

--- 804-rotated-digits/rotated-digits.py
+++ 804-rotated-digits/rotated-digits.py
@@ -2,11 +2,16 @@ class Solution:
     def rotatedDigits(self, n: int) -> int:
         count = 0
         for i in range(1, n + 1):
-            s = str(i)
-            # must not have 3,4,7
-            if any(d in s for d in "347"):
-                continue
-            # must have at least one of 2,5,6,9
-            if any(d in s for d in "2569"):
+            check = i
+            is_valid = True
+            has_changed = False
+            while check > 0:
+                digit = check % 10
+                if digit in (3, 4, 7):
+                    is_valid = False
+                elif digit in (2, 5, 6, 9):
+                    has_changed = True
+                check //= 10
+            if is_valid and has_changed:
                 count += 1
         return count
