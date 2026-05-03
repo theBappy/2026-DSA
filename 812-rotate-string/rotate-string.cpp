b51b94725e3a0866028f8ea03fbb3adb04f91892
theBappy <abappy575@gmail.com>
2026-05-03 10:10:49 +0600

    Time: 0 ms (100.00%) | Memory: 8.1 MB (61.66%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,14 +3,8 @@ public:
     bool rotateString(string s, string goal) {
         int m = s.length();
         int n = goal.length();
-        if (m != n)
-            return false;
-        for (int rotationCount = 1; rotationCount <= m; rotationCount++) {
-            // rotate(start of range, which position will become start position of range, end of range)
-            rotate(s.begin(), s.begin() + 1, s.end());
-            if (s == goal)
-                return true;
-        }
+        if(m == n && (s+s).find(goal) != string::npos)
+            return true;
         return false;
     }
 };
