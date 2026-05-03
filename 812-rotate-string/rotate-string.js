e44e3f9a571d6bb3f0e263e83f5f31c085086798
theBappy <abappy575@gmail.com>
2026-05-03 10:10:13 +0600

    Time: 3 ms (10.27%) | Memory: 55.5 MB (8.91%) - LeetSync

--- 812-rotate-string/rotate-string.js
+++ 812-rotate-string/rotate-string.js
@@ -1,6 +1,6 @@
 var rotateString = function (s, goal) {
     const m = s.length
-    const n = s.length
+    const n = goal.length
     if (m !== n) return false;
     let arr = s.split('')
     for (let rotationCount = 1; rotationCount <= m; rotationCount++) {
