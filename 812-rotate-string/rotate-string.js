f16522f7fbddbe5248f57cccd38239f9a0d96342
theBappy <abappy575@gmail.com>
2026-05-03 19:57:01 +0600

    Time: 0 ms (100.00%) | Memory: 53.1 MB (86.82%) - LeetSync

--- 812-rotate-string/rotate-string.js
+++ 812-rotate-string/rotate-string.js
@@ -1,17 +1,8 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
 var rotateString = function (s, goal) {
-    const n = s.length
-    if (n !== goal.length) return false
-
-    for (let i = 0; i < n; i++) {
-        let match = true
-        for (let j = 0; j < n; j++) {
-            // check if characters match with a wrap-around index
-            if (s[(i + j) % n] !== goal[j]) {
-                match = false
-                break
-            }
-        }
-        if (match) return true
-    }
-    return false
+    return s.length === goal.length && (s + s).includes(goal)
 };
