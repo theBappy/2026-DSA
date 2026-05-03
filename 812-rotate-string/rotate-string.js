7175d1ee9050645eddcacddda814020d6431ca96
theBappy <abappy575@gmail.com>
2026-05-03 10:07:59 +0600

    Time: 3 ms (10.27%) | Memory: 55.7 MB (6.98%) - LeetSync

---
+++ 812-rotate-string/rotate-string.js
@@ -0,0 +1,12 @@
+var rotateString = function (s, goal) {
+    const m = s.length
+    const n = s.length
+    if (m !== n) return false;
+    let arr = s.split('')
+    for (let rotationCount = 1; rotationCount <= m; rotationCount++) {
+        const firstChar = arr.shift()
+        arr.push(firstChar)
+        if (arr.join('') === goal) return true
+    }
+    return false
+};
