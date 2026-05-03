fee3a7f3995872029de1b43307079eb729e603b3
theBappy <abappy575@gmail.com>
2026-05-03 10:36:21 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (15.24%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -1,5 +1,17 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if len(s) == len(goal) and (s + s).find(goal) != -1:
-            return True
+        n = len(s)
+        if n != len(goal):
+            return False
+
+        # try every possible starting shift 'i'
+        for i in range(n):
+            match = True
+            for j in range(n):
+                # use modulo to wrap around the index of s
+                if s[(i + j) % n] != goal[j]:
+                    match = False
+                    break
+            if match:
+                return True
         return False
