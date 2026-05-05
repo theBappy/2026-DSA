8f8739cab92cd4985d5a7117ff1fa3f4f4ad0918
theBappy <abappy575@gmail.com>
2026-05-05 10:28:18 +0600

    Time: 0 ms (100.00%) | Memory: 29.5 MB (91.28%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,15 +1,21 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;
-        vector<int> rotated(n);
+    void rotate(std::vector<int>& nums, int k) {
+        k %= nums.size();
 
-        for (int i = 0; i < n; i++) {
-            rotated[(i + k) % n] = nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = rotated[i];
-        }
+        auto reverse = [&](int left, int right) {
+            while (left < right) {
+                swap(nums[left], nums[right]);
+                left++;
+                right--;
+            }
+        };
+
+        reverse(0, nums.size() - 1);
+        reverse(0, k - 1);
+        reverse(k, nums.size() - 1);
     }
 };
