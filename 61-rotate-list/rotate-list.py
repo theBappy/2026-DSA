3a1a5b47f63c2204c82fd5de8a9474289192715f
theBappy <abappy575@gmail.com>
2026-05-05 16:57:04 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (36.28%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -1,30 +1,26 @@
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head or not head.next or k == 0:
-                return head
-        
-        # 1. Compute length and find the actual tail
-        last_node = head
-        length = 1
-        while last_node.next:
-            last_node = last_node.next
-            length += 1
-        
-        # 2. Close the loop
-        last_node.next = head
-        
-        # 3. Find the split point
-        # k can be larger than length, so use modulo
-        k = k % length
-        steps_to_new_tail = length - k - 1
-        
-        new_tail = head
-        for _ in range(steps_to_new_tail):
-            new_tail = new_tail.next
-            
-        # 4. Break the circle
-        new_head = new_tail.next
-        new_tail.next = None
-        
-        return new_head
+            return head
 
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k = k % length
+        if k == 0:
+            return head
+
+        tail.next = head
+
+        steps = length - k
+        newTail = head
+        for i in range(1, steps):
+            newTail = newTail.next
+
+        newHead = newTail.next
+        newTail.next = None
+
+        return newHead
