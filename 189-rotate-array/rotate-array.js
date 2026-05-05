166e8d0838646eb62b0e4d29f5e44ab0a6c780db
theBappy <abappy575@gmail.com>
2026-05-05 10:15:13 +0600

    Time: 2 ms (59.32%) | Memory: 65.6 MB (50.99%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,14 +4,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    const n = nums.length
-    k = k % n
-    const rotated = new Array(n).fill(0)
-
-    for (let i = 0; i < n; i++) {
-        rotated[(i + k) % n] = nums[i]
-    }
-    for (let i = 0; i < n; i++) {
-        nums[i] = rotated[i]
+    k = k % nums.length
+    if (k !== 0) {
+        let temp = nums.slice(-k).concat(nums.slice(0, -k))
+        for (let i = 0; i < nums.length; i++) {
+            nums[i] = temp[i]
+        }
     }
 };
