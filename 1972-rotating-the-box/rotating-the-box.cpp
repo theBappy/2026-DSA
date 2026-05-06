5298600c09bbaf8d20c56a748dde00e33643b26e
theBappy <abappy575@gmail.com>
2026-05-06 10:49:25 +0600

    Time: 8 ms (81.33%) | Memory: 56.8 MB (69.53%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -4,32 +4,17 @@ public:
         int m = box.size();
         int n = box[0].size();
 
-        vector<vector<char>> result(n, vector<char>(m));
+        vector<vector<char>> result(n, vector<char>(m, '.'));
 
-        // transpose
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                result[i][j] = box[j][i];
-            }
-        }
-
-        // reverse(90 degree rotation)
-        for (vector<char>& row : result) {
-            reverse(begin(row), end(row));
-        }
-
-        // apply gravity
-        for (int j = 0; j < m; j++) {
-            int spaceBottomn = n - 1;
-            for (int i = n - 1; i >= 0; i--) {
-                if (result[i][j] == '*') {
-                    spaceBottomn = i - 1;
-                    continue;
-                }
-                if (result[i][j] == '#') {
-                    result[i][j] = '.';
-                    result[spaceBottomn][j] = '#';
-                    spaceBottomn--;
+        for (int i = 0; i < m; i++) {
+            int lowest_empty_space = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '#') {
+                    result[lowest_empty_space][m - 1 - i] = '#';
+                    lowest_empty_space--;
+                } else if (box[i][j] == '*') {
+                    result[j][m - 1 - i] = '*';
+                    lowest_empty_space = j - 1;
                 }
             }
         }
