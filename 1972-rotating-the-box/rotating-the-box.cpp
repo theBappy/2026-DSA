7f7bdede24510f985552062ee70bb6a0fcee5cd4
theBappy <abappy575@gmail.com>
2026-05-06 10:32:57 +0600

    Time: 7 ms (87.22%) | Memory: 56.8 MB (38.33%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -6,41 +6,33 @@ public:
 
         vector<vector<char>> result(n, vector<char>(m));
 
-        //Transpose
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        // transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 result[i][j] = box[j][i];
             }
         }
 
-        //90 degree k lie - Reverse kardo ahr row ko
-        for(vector<char>& row : result) {
+        // reverse(90 degree rotation)
+        for (vector<char>& row : result) {
             reverse(begin(row), end(row));
         }
 
-        //Apply the gravity
-        for(int j = 0; j < m; j++) {
-            for(int i = n-1; i >= 0; i--) {
-                if(result[i][j] == '.') { //if it is a space
-                    int stoneRow = -1;
-
-                    for(int k = i-1; k >= 0; k--) {
-                        if(result[k][j] == '*') {
-                            break;
-                        } else if(result[k][j] == '#') {
-                            stoneRow = k;
-                            break;
-                        }
-                    }
-
-                    if(stoneRow != -1) {
-                        result[i][j] = '#';
-                        result[stoneRow][j] = '.';
-                    }
+        // apply gravity
+        for (int j = 0; j < m; j++) {
+            int spaceBottomn = n - 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (result[i][j] == '*') {
+                    spaceBottomn = i - 1;
+                    continue;
+                }
+                if (result[i][j] == '#') {
+                    result[i][j] = '.';
+                    result[spaceBottomn][j] = '#';
+                    spaceBottomn--;
                 }
             }
         }
-
         return result;
     }
 };
