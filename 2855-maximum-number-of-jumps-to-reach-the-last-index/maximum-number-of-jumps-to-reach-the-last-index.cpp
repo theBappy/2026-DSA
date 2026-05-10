f4d5d389bf4f1664b699af3e69f2e4a1bc877b0f
theBappy <abappy575@gmail.com>
2026-05-10 10:22:02 +0600

    Time: 27 ms (27.33%) | Memory: 68.5 MB (69.25%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int n;
-    int solve(int i, vector<int>& nums, int target, vector<int>& t) {
-        if (i == n - 1)
-            return t[i] = 0; // no more steps, reached destination
-        if (t[i] != INT_MIN)
-            return t[i];
-        int result = INT_MIN;
-        for (int j = i + 1; j < n; j++) {
-            if (abs(nums[j] - nums[i]) <= target) {
-                int temp = 1 + solve(j, nums, target, t);
-                result = max(temp, result);
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> t(n + 1, INT_MIN);
+        t[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (abs(nums[j] - nums[i]) <= target && t[j] != INT_MIN) {
+                    int temp = 1 + t[j];
+                    t[i] = max(t[i], temp);
+                }
             }
         }
-        return t[i] = result;
-    }
-    int maximumJumps(vector<int>& nums, int target) {
-        n = nums.size();
-        vector<int> t(n + 1, INT_MIN);
-        int result = solve(0, nums, target, t);
-
-        return result < 0 ? -1 : result;
+        return t[0] < 0 ? -1 : t[0];
     }
 };
