5f3a3b3f9e618768c7e63a0368e9bda87fbfb882
theBappy <abappy575@gmail.com>
2026-05-11 08:40:43 +0600

    Time: 0 ms (100.00%) | Memory: 51.7 MB (100.00%) - LeetSync

--- 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.js
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.js
@@ -1,19 +1,8 @@
-/**
- * @param {number} num
- * @return {number}
- */
 var countEven = function (num) {
-    let count = 0;
-    for (let i = 1; i <= num; i++) {
-        let num = i;
-        let sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num = Math.floor(num / 10);
-        }
-        if (sum % 2 === 0) {
-            count++;
-        }
+    let dSum = 0, temp = num
+    while (temp > 0) {
+        dSum += temp % 10
+        temp = Math.floor(temp / 10)
     }
-    return count;
+    return (dSum % 2 === 0) ? Math.floor(num / 2) : Math.floor((num - 1) / 2)
 };
