dc8f1fb6661dc6d81663a886d638af3c24eabc24
theBappy <abappy575@gmail.com>
2026-05-12 10:58:10 +0600

    Time: 38 ms (53.85%) | Memory: 86.5 MB (7.69%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.js
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.js
@@ -1,35 +1,13 @@
 var minimumEffort = function (tasks) {
-    let left = 0
-    let right = 1e9
-    let result = Number.MAX_SAFE_INTEGER
+    // Sort by difference (min - actual) descending
+    tasks.sort((task1, task2) => (task2[1] - task2[0]) - (task1[1] - task1[0]))
 
-    const doAble = (mid) => {
-        for (const task of tasks) {
-            const actualEnergyNeeds = task[0];
-            const minEnergyNeeds = task[1];
+    let totalNeeded = 0
+    let currentSpent = 0
 
-            if (minEnergyNeeds > mid) {
-                return false;
-            }
-            mid -= actualEnergyNeeds;
-        }
-        return true;
+    for (const [actual, minimum] of tasks) {
+        totalNeeded = Math.max(totalNeeded, currentSpent + minimum)
+        currentSpent += actual
     }
-
-    tasks.sort((task1, task2) => {
-        const diff1 = task1[1] - task1[0]
-        const diff2 = task2[1] - task2[0]
-        return diff2 - diff1
-    })
-
-    while (left <= right) {
-        const mid = Math.floor(left + (right - left) / 2);
-        if (doAble(mid)) {
-            result = mid;
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    }
-    return result
+    return totalNeeded
 };
