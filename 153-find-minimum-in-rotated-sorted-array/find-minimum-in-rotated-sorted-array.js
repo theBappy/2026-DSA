5dc39ac301cb53fa03efad4b5b2e02d80b7aa817
theBappy <abappy575@gmail.com>
2026-05-15 10:29:25 +0600

    Time: 0 ms (100.00%) | Memory: 53 MB (88.77%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,12 @@
+var findMin = function (nums) {
+    let left = 0, right = nums.length - 1
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2)
+        if (nums[mid] > nums[right]) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    return nums[right]
+};
