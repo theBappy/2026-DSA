732efa8df24105599aeac09198729b731149e3f9
theBappy <abappy575@gmail.com>
2026-05-17 11:50:44 +0600

    Time: 16 ms (20.47%) | Memory: 37.9 MB (27.07%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -14,8 +14,11 @@ class Solution:
             # mark as visited by flipping the sign
             arr[i] *= -1
 
-            left = dfs(i - arr[i])
-            right = dfs(i + arr[i])
+            # arr[i] is now negative, so:
+            # i - arr[i] moves right (original i + value)
+            # i + arr[i] moves left  (original i - value)
+            left = dfs(i + arr[i])
+            right = dfs(i - arr[i])
 
             return left or right
 
