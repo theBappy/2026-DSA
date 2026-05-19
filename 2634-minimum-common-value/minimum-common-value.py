e915454cdb264a05d5797fa79147f6ab4776c4d7
theBappy <abappy575@gmail.com>
2026-05-19 10:15:49 +0600

    Time: 132 ms (5.03%) | Memory: 37.7 MB (79.31%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,7 +1,22 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        set1 = set(nums1)
-        for num in nums2:
-            if num in set1:
+        def binary_search(target, nums):
+            left = 0
+            right = len(nums) - 1
+
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    return True
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            return False
+
+        for num in nums1:
+            if binary_search(num, nums2):
                 return num
+
         return -1
