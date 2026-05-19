a71f1d934f5b2b21e874e91116b9b50412d6b024
theBappy <abappy575@gmail.com>
2026-05-19 10:29:15 +0600

    Time: 2 ms (52.88%) | Memory: 62.2 MB (95.19%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.js
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -1,22 +1,13 @@
 var getCommon = function (nums1, nums2) {
-    function binarySearch(target, nums) {
-        let left = 0
-        let right = nums.length - 1
-        while (left <= right) {
-            const mid = left + Math.floor((right - left) / 2);
-            if (nums[mid] === target) {
-                return true
-            } else if (nums[mid] < target) {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-        }
-        return false
-    }
-    for (const num of nums1) {
-        if (binarySearch(num, nums2)) {
-            return num
+    let i = 0
+    let j = 0
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            return nums1[i]
+        } else if (nums1[i] < nums2[j]) {
+            i++
+        } else {
+            j++
         }
     }
     return -1
