c07c8e55cc0734afd605f0edd4e5f872fece697e
theBappy <abappy575@gmail.com>
2026-05-19 10:22:13 +0600

    Time: 0 ms (100.00%) | Memory: 54.6 MB (30.11%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-
-        while (l <= r) {
-
-            int mid = l + (r - l) / 2;
-
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return false;
-    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        for (int& num : nums1) {
-            if (binarySearch(nums2, num)) {
-                return num;
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         return -1;
