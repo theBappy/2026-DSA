783fcc0ba07f59436ee2132a84bae9586e5bd53d
theBappy <abappy575@gmail.com>
2026-05-19 10:12:24 +0600

    Time: 7 ms (27.28%) | Memory: 54.6 MB (58.81%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
+    bool binarySearch(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        for (int& num : nums2) {
-            if (set1.contains(num))
+        for (int& num : nums1) {
+            if (binarySearch(nums2, num)) {
                 return num;
+            }
         }
         return -1;
     }
