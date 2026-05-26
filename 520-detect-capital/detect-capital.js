72c7bb53db688a51f22ebdc25e12ff973e76a362
theBappy <abappy575@gmail.com>
2026-05-26 10:08:30 +0600

    Time: 0 ms (100.00%) | Memory: 54.3 MB (66.88%) - LeetSync

---
+++ 520-detect-capital/detect-capital.js
@@ -0,0 +1,3 @@
+var detectCapitalUse = function (word) {
+    return word === word.toUpperCase() || word === word.toLowerCase() || word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase()
+};
