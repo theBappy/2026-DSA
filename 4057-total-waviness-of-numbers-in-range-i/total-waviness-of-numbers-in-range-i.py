838c744c1a3ebe7d13c2cd08703bb38a8d4e12f4
theBappy <abappy575@gmail.com>
2026-06-04 10:12:13 +0600

    Time: 503 ms (8.05%) | Memory: 19.3 MB (32.95%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -1,19 +1,7 @@
 class Solution:
-    MAX_LIMIT = 100005
-    waviness_counts = [0] * MAX_LIMIT
-    prefix_sums = [0] * MAX_LIMIT
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        def waviness(n: int) -> int:
+            s = str(n)
+            return sum((a < b > c) or (a > b < c) for a, b, c in zip(s, s[1:], s[2:]))
 
-    for num in range(100, MAX_LIMIT):
-        units_digit = num % 10
-        tens_digit = (num // 10) % 10
-        hundreds_digit = (num // 100) % 10  
-
-        is_peak = tens_digit > max(units_digit, hundreds_digit)
-        is_valley = tens_digit < min(units_digit, hundreds_digit)
-        has_new_wave = int(is_peak or is_valley)
-
-        waviness_counts[num] = waviness_counts[num // 10] + has_new_wave
-        prefix_sums[num] = prefix_sums[num - 1] + waviness_counts[num]
-
-    def totalWaviness(self, start_num: int, end_num: int) -> int:
-        return self.prefix_sums[end_num] - self.prefix_sums[start_num - 1]
+        return sum(waviness(n) for n in range(num1, num2 + 1))
