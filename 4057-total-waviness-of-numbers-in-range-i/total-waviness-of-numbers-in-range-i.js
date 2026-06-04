30a41f6d51647d0f4f661cb8b5b9c849e9c9830a
theBappy <abappy575@gmail.com>
2026-06-04 10:11:00 +0600

    Time: 21 ms (78.26%) | Memory: 62.5 MB (30.43%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.js
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.js
@@ -1,19 +1,19 @@
-const MAX_LIMIT = 100005;
-const waviness_counts = new Int32Array(MAX_LIMIT);
-const prefix_sums = new Int32Array(MAX_LIMIT);
-
-for (let num = 100; num < MAX_LIMIT; num++) {
-    const units_digit = num % 10;
-    const tens_digit = Math.floor(num / 10) % 10;
-    const hundreds_digit = Math.floor(num / 100) % 10;
-
-    const is_peak = tens_digit > Math.max(units_digit, hundreds_digit);
-    const is_valley = tens_digit < Math.min(units_digit, hundreds_digit);
-    const has_new_wave = (is_peak || is_valley) ? 1 : 0;
-
-    waviness_counts[num] = waviness_counts[Math.floor(num / 10)] + has_new_wave;
-    prefix_sums[num] = prefix_sums[num - 1] + waviness_counts[num];
-}
-var totalWaviness = function (start_num, end_num) {
-    return prefix_sums[end_num] - prefix_sums[start_num - 1];
+var totalWaviness = function (num1, num2) {
+    const getWaviness = x => {
+        const s = x.toString()
+        let waviness = 0
+        for (let i = 1; i < s.length - 1; i++) {
+            const isPeak = s[i] > s[i - 1] && s[i] > s[i + 1]
+            const isValley = s[i] < s[i - 1] && s[i] < s[i + 1]
+            if (isPeak || isValley) {
+                waviness++
+            }
+        }
+        return waviness
+    }
+    let total = 0
+    for (let i = num1; i <= num2; i++) {
+        total += getWaviness(i)
+    }
+    return total
 };
