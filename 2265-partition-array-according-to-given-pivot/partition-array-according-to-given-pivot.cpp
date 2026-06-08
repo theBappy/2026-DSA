3c27a3c5efc5366e6156fcf382ceea1b5563fc63
theBappy <abappy575@gmail.com>
2026-06-08 11:11:29 +0600

    Time: 4 ms (81.51%) | Memory: 127.8 MB (78.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    vector<int> pivotArray(vector<int>& nums, int pivot) {
+    vector<int> pivotArray(std::vector<int>& nums, int pivot) {
         int n = nums.size();
         vector<int> result(n);
 
-        // original nums pointers
-        int i = 0;
-        int j = n - 1;
+        // Original nums pointers
+        int left_idx = 0;
+        int right_idx = n - 1;
 
-        // result array pointers
-        int i_ = 0;
-        int j_ = n - 1;
+        // Result array destination pointers
+        int res_left = 0;
+        int res_right = n - 1;
 
-        while (i < n && j >= 0) {
-            if (nums[i] < pivot) {
-                result[i_] = nums[i];
-                i_++;
+        while (left_idx < n && right_idx >= 0) {
+            if (nums[left_idx] < pivot) {
+                result[res_left] = nums[left_idx];
+                res_left++;
             }
-            if (nums[j] > pivot) {
-                result[j_] = nums[j];
-                j_--;
+            if (nums[right_idx] > pivot) {
+                result[res_right] = nums[right_idx];
+                res_right--;
             }
-            i++;
-            j--;
+            left_idx++;
+            right_idx--;
         }
-        while (i_ <= j_) {
-            result[i_] = pivot;
-            i_++;
+
+        // Fill the remaining middle elements with pivot
+        while (res_left <= res_right) {
+            result[res_left] = pivot;
+            res_left++;
         }
+
         return result;
     }
 };
