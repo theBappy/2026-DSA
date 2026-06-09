0dcda0a5fc08440a61dde6de78af9cbcc8b3fd86
theBappy <abappy575@gmail.com>
2026-06-09 08:48:37 +0600

    Time: 2 ms (100.00%) | Memory: 67.7 MB (50.00%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.js
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.js
@@ -1,8 +1,3 @@
 var maxTotalValue = function (nums, k) {
-    let m1 = Infinity, m2 = -Infinity
-    for (const x of nums) {
-        m1 = Math.min(m1, x)
-        m2 = Math.max(m2, x)
-    }
-    return (m2 - m1) * k
+    return (Math.max(...nums) - Math.min(...nums)) * k
 };
