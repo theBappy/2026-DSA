b48aa62e8821488cb5c9fa2cc68ee3afc7bdd8f4
theBappy <abappy575@gmail.com>
2026-06-30 09:59:57 +0600

    Time: 59 ms (92.88%) | Memory: 19.3 MB (85.27%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,8 +1,9 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        res = 0
-        p = [-1, -1, -1]
-        for i, ch in enumerate(s):
-            p[(ord(ch) & 31) - 1] = i
-            res += min(p) + 1
-        return res
+        last_pos = [-1] * 3
+        total = 0
+
+        for pos in range(len(s)):
+            last_pos[ord(s[pos]) - ord("a")] = pos
+            total += 1 + min(last_pos)
+        return total
