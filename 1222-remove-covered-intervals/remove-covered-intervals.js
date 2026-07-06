3c0bb89537ad4e64c3b3ede370093466f01577ba
theBappy <abappy575@gmail.com>
2026-07-06 10:02:25 +0600

    Time: 4 ms (74.47%) | Memory: 57.9 MB (31.91%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.js
@@ -0,0 +1,20 @@
+var removeCoveredIntervals = function(intervals) {
+    intervals.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return b[1] - a[1];
+        }
+        return a[0] - b[0];
+    });
+
+    let count = 0;
+    let maxEnd = 0;
+
+    for (const interval of intervals) {
+        if (interval[1] > maxEnd) {
+            count++;
+            maxEnd = interval[1];
+        }
+    }
+
+    return count;
+};
