51872706cc936d078238e959648e12f7c1572d74
theBappy <abappy575@gmail.com>
2026-08-03 10:05:05 +0600

    Time: 60 ms (37.04%) | Memory: 72.7 MB (25.93%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.js
@@ -0,0 +1,37 @@
+var stoneGameIII = function (stoneValue) {
+    const n = stoneValue.length;
+    const t = new Array(n + 1).fill(-1);
+
+    function solve(i) {
+        if (i === n) {
+            return 0;
+        }
+
+        if (t[i] !== -1) {
+            return t[i];
+        }
+
+        t[i] = stoneValue[i] - solve(i + 1);
+
+        if (i + 1 < n) {
+            t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i + 1] - solve(i + 2));
+        }
+
+        if (i + 2 < n) {
+            t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - solve(i + 3));
+        }
+
+        return t[i];
+    }
+
+    const diff = solve(0);
+
+    if (diff < 0) {
+        return "Bob";
+    } else if (diff > 0) {
+        return "Alice";
+    }
+
+    return "Tie";
+
+};
